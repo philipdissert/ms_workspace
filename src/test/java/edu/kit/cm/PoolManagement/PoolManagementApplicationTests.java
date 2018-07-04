2dfd7e5a72850a963c151de6dbfc53515b5680d4
Philip <philip.dissert@gmail.com>
2018-07-04 17:03:59 +0200

    test change

--- src/test/java/edu/kit/cm/PoolManagement/PoolManagementApplicationTests.java
+++ src/test/java/edu/kit/cm/PoolManagement/PoolManagementApplicationTests.java
@@ -2,12 +2,13 @@ package edu.kit.cm.PoolManagement;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import edu.kit.cm.WorkspaceManagement.PoolManagementApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes={SpringBootApplication.class})
+@SpringBootTest(classes={PoolManagementApplication.class})
 public class PoolManagementApplicationTests {
 
 	@Test
