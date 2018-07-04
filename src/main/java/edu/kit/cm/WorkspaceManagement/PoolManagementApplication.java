432fd83d0752a016a96d093b3a16b1f1796e316a
Philip <philip.dissert@gmail.com>
2018-07-04 17:00:30 +0200

    test change

--- src/main/java/edu/kit/cm/WorkspaceManagement/PoolManagementApplication.java
+++ src/main/java/edu/kit/cm/WorkspaceManagement/PoolManagementApplication.java
@@ -2,7 +2,6 @@ package edu.kit.cm.WorkspaceManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class PoolManagementApplication {
