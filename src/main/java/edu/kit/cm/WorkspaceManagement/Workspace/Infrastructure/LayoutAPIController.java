cc6237deefdb2b15a91048ebd8678584ab5a828e
steffen <steffen.schmitt@student.kit.edu>
2018-08-27 18:42:29 +0200

    add layout changed to add-layout/id/{id}

--- src/main/java/edu/kit/cm/WorkspaceManagement/Workspace/Infrastructure/LayoutAPIController.java
+++ src/main/java/edu/kit/cm/WorkspaceManagement/Workspace/Infrastructure/LayoutAPIController.java
@@ -83,8 +83,9 @@ public class LayoutAPIController {
 		return workspaceAdapter.getLayout().toString();
 	}
 
-	@PostMapping("/add-layout")
-	public void addLayout(@RequestBody String input) {
+	@PostMapping("/add-layout/id/{id}")
+	public void addLayout(@RequestBody String input,@PathVariable int id) {
+		System.out.println(input);
 		JSONObject jsonObject = new JSONObject(input);
 		workspaceAdapter.addLayout(jsonObject);
 	}
